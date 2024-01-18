7411bf024be4f5ab32d6234dc7f350345e095bf9
LivingKnowLedge <hutbrand@sheridancollege.ca>
2024-01-18 02:11:26 -0500

    Task 4b Complete: make a collision hide and unhide another game object

---
+++ Assets/Week 2/Scripts/Lever.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("test");
+
+        if (door.activeInHierarchy) {
+            door.SetActive(false);
+        }
+        else { 
+        door.SetActive(true);
+        }
+    } 
+}
